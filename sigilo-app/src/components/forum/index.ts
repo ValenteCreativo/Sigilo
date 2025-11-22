8f9f18507df94d874d4e61c3f6549f3e85a368ea
Valentín Martínez <valentecreativo@proton.me>
2025-11-22 20:31:37 -0300

    feat(forum): setup forum component exports

--- sigilo-app/src/components/forum/index.ts
+++ sigilo-app/src/components/forum/index.ts
@@ -2,4 +2,5 @@ export { RoleBadge } from "./RoleBadge";
 export { StatusBadge } from "./StatusBadge";
 export { MethodPill } from "./MethodPill";
 export { ReportNetwork } from "./ReportNetwork";
+export { ObsidianNetwork } from "./ObsidianNetwork";
 export { ReportList } from "./ReportList";
